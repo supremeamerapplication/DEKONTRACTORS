1deb86e67f7d16f0fb350f0eae286d03b8f6a819
supremeamerapplication <supremeamerapplication@gmail.com>
2026-01-18 07:58:36 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,89 @@
+// Service Worker for Dekontractors PWA
+const CACHE_NAME = 'dekontractors-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './file_00000000d3d4720ab491ef5cc601a455.png',
+  './panash-designs-artstation-post-01.jpg',
+  './capi_dfa945349ded659b9dc39dcb4805fca6_c13e948a7a72748323ef771bf8aff689.jpeg',
+  './360_F_224037581_KsgpR416w2iOpG0AaIr3d9Kaqpt4jgft.webp',
+  './IMG-20260113-WA0004.jpg'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened, adding files...');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log('All resources cached');
+        return self.skipWaiting();
+      })
+      .catch(error => {
+        console.error('Cache installation failed:', error);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('Service Worker activated');
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        if (response) {
+          console.log('Serving from cache:', event.request.url);
+          return response;
+        }
+        console.log('Fetching from network:', event.request.url);
+        return fetch(event.request).then(response => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+              console.log('Cached new resource:', event.request.url);
+            });
+
+          return response;
+        });
+      })
+      .catch(() => {
+        // If both cache and network fail, show offline page
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      })
+  );
+});
